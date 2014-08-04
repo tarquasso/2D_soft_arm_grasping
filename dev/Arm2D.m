b62d0b689fd15819c293652fed7fac6fb2f0dc20
AndyMarchese <andy@csail.mit.edu>
2014-08-03 23:18:17 -0400

    added forkin and plot methods to 2D arm obj

--- dev/Arm2D.m
+++ dev/Arm2D.m
@@ -1,23 +1,20 @@
 classdef Arm2D < handle
-    %ARM Summary of this class goes here
-    %   Detailed explanation goes here
-    
+   
     properties
         dims                     % Arm dimensions
         gripper2D;               % Gripper2D
         curvatureController      % curvature controller
     end
     properties(SetAccess=private,GetAccess=public)
-        L = [];                  % is the current/measured length vector <--- MEASURE FROM MOCAP INITIAL FRAME
-        kMeasured               % measured arc curvatures
-        kTarget                % target arc curvatures      
+        L                        % is the current/measured length vector <--- MEASURE FROM MOCAP INITIAL FRAME
+        kMeasured                % measured arc curvatures
+        kTarget                  % target arc curvatures      
     end
    
     methods
         %Constructor
         function obj = Arm2D()
             % Dimensions and orientation of the arm
-            
             obj.dims = struct();
             obj.dims.S = 6;      % is the total number of arm segments
             obj.dims.kMin = -20;     % minimum allowable curvature
@@ -32,7 +29,7 @@ classdef Arm2D < handle
             %Create gripper2D before curvature controller
             obj.gripper2D = Gripper2D;
             %Create a curvature controller
-            obj.curvatureController = CurvatureController;
+            %obj.curvatureController = CurvatureController;
 
         end
         %Destructor
@@ -55,8 +52,8 @@ classdef Arm2D < handle
                 
                 if( strcmp(above_max, 'false') )
                     obj.kTarget = val;
-                    obj.curvatureController.sendCurvatureErrors(...
-                        obj.kTarget', obj.kMeasured);
+                    %obj.curvatureController.sendCurvatureErrors(...
+                    %    obj.kTarget', obj.kMeasured);
                 else
                     error('An element of k exceeds allowable limit')
                 end
@@ -65,47 +62,41 @@ classdef Arm2D < handle
                 error('The size of k does not match the arm.')
             end
         end
-        
+        %Set Measured Curvatures of the 2D arm
         function setMeasuredCurvatures(obj, val)
             l_valSize = size(val);
-            if( l_valSize(1) == obj.N && l_valSize(2) == 1)
+            if( l_valSize(1) == obj.dims.S && l_valSize(2) == 1)
                 obj.kMeasured = val;
             else
                 error('The size of measured k does not match the arm.');
             end
         end
-        
+        %Set Measured Lengths of the 2D arm
         function setMeasuredLengths(obj, val)
             l_valSize = size(val);
-            if( l_valSize(1) == obj.N && l_valSize(2) == 1)
+            if( l_valSize(1) == obj.dims.S && l_valSize(2) == 1)
                 obj.L = val;
             else
                 error('The size of measured L does not match the arm.');
             end
         end
-        
-        function [x, y, theta] = recursiveForwardKinematics(obj, gripper_on, k, i, s)
+        %Forward kinematic transformation of the 2D arm 
+        function [x, y, theta] = recursiveForwardKinematics(obj, k, i, s)
             
             % gripper_on - boolean if gripper2D attached
             % k - either measured or target curvature vector
             % i - is the segment of interest
             % s - is the length of interest along segment i
-            
+
+            l_N = obj.dims.S + 1; % number of arm links plus the gripper
+            l_L = [obj.L; obj.gripper2D.L];  
             l_valSize = size(k);
-            if( l_valSize(1) ~= obj.N || l_valSize(2) ~= 1)
-                error('The size of measured k does not match the arm.');
+
+            if( i > l_valSize(1) )
+                error('The size of k does not match the segment of interest.');
             end
             
-            if ( gripper_on )
-                l_N = obj.N + 1;
-                l_L = [obj.L; obj.gripper2D.L];
-                k = [obj.kMeasured; obj.gripper2D.k];
-            else
-                l_N = obj.N;
-                l_L = obj.L;
-            end
-            
-            l_theta0 = obj.theta0;
+            l_theta0 = obj.dims.theta0;
             
             theta_init = zeros(1,l_N);
             x_init = zeros(1,l_N);
@@ -116,7 +107,7 @@ classdef Arm2D < handle
                 x_init(i) = 0.0;
                 y_init(i) = 0.0;
             else
-                [x_init(i), y_init(i), theta_init(i)] = obj.recursiveForwardKinematics(obj, gripper_on, k, i-1, l_L(i-1));
+                [x_init(i), y_init(i), theta_init(i)] = obj.recursiveForwardKinematics(k, i-1, l_L(i-1) );
             end
             
             theta = theta_init(i) + k(i)*s;
@@ -124,29 +115,32 @@ classdef Arm2D < handle
             y = y_init(i) - cos(theta)/k(i) + cos(theta_init(i))/k(i);
             
         end
-        
-        function h = plotArmToHandle(obj, gripper_on)
+        %Plot the measured state of the 2D
+        function h = plotArmToHandle(obj)
             
+            l_N = obj.dims.S + 1;
+            l_L = [obj.L; obj.gripper2D.L];
+            l_k = [obj.kMeasured; obj.gripper2D.kMeasured];
+
             M = 20;
             total = 1;
-            x = zeros(1, N*M);
-            y = zeros(1, N*M);
-            theta = zeros(1, N*M);
-            
-            for i=1:N
+            x = zeros(1, l_N*M);
+            y = zeros(1, l_N*M);
+            theta = zeros(1, l_N*M);
+
+            for i=1:l_N
                 for j=1:M
-                    [x(total), y(total), theta(total)] = ...
-                        recursiveForwardKinematics(obj, gripper_on, k, i, ...
-                        obj.L(i)*(j/M));
+                    [x(total), y(total), theta(total)] = obj.recursiveForwardKinematics( l_k, i, l_L(i)*(j/M));
                     total = total + 1;
                 end
             end
-            
+
             hold on
             axis([-0.30 0.30 -0.10 0.50])
             axis square
-            
+
             h = plot(x(1:end-20),y(1:end-20), 'r', x(end-20:end),y(end-20:end), 'k', 'LineWidth', 2);
+            drawnow;
             
         end
         
