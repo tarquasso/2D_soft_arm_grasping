6128adfcb9cf7f7f1774713af9ebe54465ae7925
AndyMarchese <andy@csail.mit.edu>
2014-08-05 09:08:48 -0400

    Updated round object

--- dev/RoundObject.m
+++ dev/RoundObject.m
@@ -1,16 +1,37 @@
 classdef RoundObject < handle
-    %ROUNDOBJECT Summary of this class goes here
-    %   Detailed explanation goes here
+
+    properties(SetAccess=private,GetAccess=public)
+        x;          % x coordinate of the center of the object  <--- MEASURE FROM MOCAP INITIAL FRAME
+        y;          % y coordinate of the center of the object  <--- MEASURE FROM MOCAP INITIAL FRAME
+        r;          % r is the radius of the circle             <--- MEASURE FROM MOCAP INITIAL FRAME
+    end
     
     properties
-        fun = 1;
-        prop = 2;
+        placed;
     end
     
     methods
         function obj = RoundObject()
-            obj.prop = 2;
+            obj.placed = 0;
         end
+        
+        function setMeasuredState(obj, val_x, val_y, val_r)
+            obj.x = val_x;
+            obj.y = val_y;
+            obj.r = val_r;
+        end
+        
+        function h = plotObjectToHandle(obj)
+            
+            delta_angle = 0.01;
+            
+            ang = 0:delta_angle:2*pi; 
+            xp = obj.r*cos(ang);
+            yp = obj.r*sin(ang);
+            h = plot(obj.x+xp,obj.y+yp,'-b'); 
+            
+        end
+        
         function delete(obj)
             
         end
