d505112e62028d6fd0e08a52721d70c2e91e9661
Curvature Controller <andy@csail.mit.edu>
2014-09-24 18:16:17 -0400

    updatet piston controller and 2 alternative approaches, which both do not work as good.

---
+++ dev/controller computer/plotResults.m
@@ -0,0 +1,12 @@
+figure;
+hold on;
+plot(mtr6_1(:,1),-(mtr6_1(:,2)-1000),'r');
+
+plot(mtr6_2(:,1),mtr6_2(:,2)-1000,'b');
+
+plot(ce6(:,1),ce6(:,2),'k');
+
+figure;
+hold on;
+plot(e6(:,1),e6(:,2),'r');
+plot(k6(:,1),k6(:,2),'k');
